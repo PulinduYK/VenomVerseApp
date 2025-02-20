52f8223742fb5a0bad9e2c19c2f6f8a641fde5fc
Thevindu Guruge <tnmdc2003@gmail.com>
2025-02-20 12:13:38 +0530

    Update settings page
    Modify settings page to connect firebase

--- App/lib/Profile/pages/settings_page.dart
+++ App/lib/Profile/pages/settings_page.dart
@@ -1,169 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:venomverse/Profile/pages/account_details_page.dart';
 import 'package:venomverse/Profile/pages/help_page.dart';
 import 'package:venomverse/Profile/pages/history_page.dart';
+import 'package:venomverse/Profile/pages/settings_edit_page.dart';
 import 'package:venomverse/Profile/widgets/profile_page_template.dart';
 
 
-// SettingsPage displays the settings details.
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
 
+
+
+// SettingsPage displays the settings details.
+class SettingsPage extends StatefulWidget {
+  final String name;
+  final String username;
+  final String? profileImage;
+
+  const SettingsPage({
+    super.key,
+    required this.name,
+    required this.username,
+    this.profileImage,
+  });
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return ProfilePageTemplate(
-        title: "Settings",
-        contentHeightFactor: 0.85,
-        child: Column(
-          children:[
-                SizedBox(height: 20),// For Space
-                Container(
-                  width: 125,
-                  height: 125,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color(0xFFD9D9D9), // Placeholder color for the profile pic
-                  ),
-                ),
-                SizedBox(height: 20), // For Space
-                Text(
-                  "John Collins", // Text for name
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize:  MediaQuery.of(context).size.width > 375 ? 24 : 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Inria Sans',
-                  ),
-                ),
-                Text(
-                  "@john_collins", // Text for username
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize:  MediaQuery.of(context).size.width > 375 ? 14 : 10,
-                    fontWeight: FontWeight.normal,
-                    fontFamily: 'Inria Sans',
-                  ),
-                ),
-                SizedBox(height: 20), // For Space
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(MediaQuery.of(context).size.width * 0.35, 60),
-                          textStyle: TextStyle(
-                              fontSize:  MediaQuery.of(context).size.width > 375 ? 24 : 18,
-                              fontWeight: FontWeight.bold
-                          ),
-                          elevation: 5,
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.black,
-                        ),
-                        onPressed: () {
-
-                        },
-                        child: Text(
-                          "Edit",
-                        )
+      title: "Settings",
+      contentHeightFactor: 0.85,
+      child: Column(
+        children:[
+          SizedBox(height: MediaQuery.of(context).size.width * 0.10),// For Space// For Space
+          CircleAvatar(
+            radius: 62.5, // 125 / 2
+            backgroundColor: Color(0xFFD9D9D9), // Placeholder color
+            backgroundImage: widget.profileImage != null && widget.profileImage!.isNotEmpty
+                ? NetworkImage(widget.profileImage!) as ImageProvider
+                : const AssetImage('assets/profile_picture.jpg'),
+          ),
+          SizedBox(height: 20), // For Space
+          Text(
+            widget.name,
+            style: GoogleFonts.inriaSans(
+              color: Colors.black,
+              fontSize:  MediaQuery.of(context).size.width > 350 ? 26 : 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '@${widget.username}',
+            style: GoogleFonts.inriaSans(
+              color: Colors.black,
+              fontSize:  MediaQuery.of(context).size.width > 350 ? 16 : 14,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.width * 0.05), // For Space
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(MediaQuery.of(context).size.width * 0.25, 40),
+                    textStyle: GoogleFonts.inriaSans(
+                        fontSize:  MediaQuery.of(context).size.width > 350 ? 24 : 18,
+                        fontWeight: FontWeight.bold
                     ),
-                    SizedBox(width: 10), // For Space between buttons
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(60, 60),
-                          maximumSize: Size(60, 60),
-                          textStyle: TextStyle(
-                              fontSize:  MediaQuery.of(context).size.width > 375 ? 24 : 18,
-                              fontWeight: FontWeight.bold
-                          ),
-                          elevation: 5,
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.black,
-                        ),
-                        onPressed: () {
-                        },
-                        child: Text(
-                          "+",
-
-                        )
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                CustomButton(
-                  text: "Account details",
-                  onPressed: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AccountDetailsPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-                CustomButton(
-                  text: "Security and Help",
-                  onPressed: (){
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HelpPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-                CustomButton(
-                  text: "History",
-                  onPressed: (){
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HistoryPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-                // create Container for add that gradient effect
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFF1C16B9), // 0%
-                          Color(0xFF6D5FD5), // 50%
-                          Color(0xFF8A7FD6), // 100%
-                        ],
-                        stops: [0.0, 0.5, 1.0], // Gradient stops
-                        begin: Alignment.topLeft,
-                        end: Alignment.topRight,
-
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 5),
-                          blurRadius: 10,
-
-                        ),
-                      ],
+                    elevation: 5,
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
                   ),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(MediaQuery.of(context).size.width * 0.5, 60),
-                        textStyle: TextStyle(
-                          fontSize:  MediaQuery.of(context).size.width > 375 ? 24 : 18,
-                          fontWeight: FontWeight.normal,
-                        ),
-                        elevation: 5,
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.white,
-                      ),
-                      onPressed: () {
-                      },
-                      child: Text(
-                        "Log out",
-                        textAlign: TextAlign.left,
-                      )
-                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsEditPage()),
+                    );
+                  },
+                  child: Text(
+                    "Edit",
+                  )
+              ),
+              SizedBox(width: 10), // For Space between buttons
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(40, 40), // Adjusted size for better visibility
+                  maximumSize: const Size(40, 40),
+                  padding: EdgeInsets.zero, // Remove extra padding
+                  shape: const CircleBorder(), // Makes it a perfect circle
+                  elevation: 5,
+                  backgroundColor: Colors.white,
+                  // foregroundColor: Colors.black,
+                ),
+                onPressed: () {},
+                child: const Icon(
+                  Icons.add,
+                  size: 26,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+          CustomButton(
+            text: "Account details",
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AccountDetailsPage()),
+              );
+            },
+          ),
+          SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+          CustomButton(
+            text: "Security and Help",
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HelpPage()),
+              );
+            },
+          ),
+          SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+          CustomButton(
+            text: "History",
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HistoryPage()),
+              );
+            },
+          ),
+          SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+          // create Container for add that gradient effect
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFB9161C), // 0%
+                  Color(0xFFF28E8E), // 100%
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.topRight,
+
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  offset: const Offset(0, 5),
+                  blurRadius: 10,
+
                 ),
               ],
+              border: Border.all( // Adding stroke
+                color: Colors.redAccent, // Change to desired stroke color
+                width: 2, // Adjust thickness
+              ),
             ),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(MediaQuery.of(context).size.width * 0.3, 50),
+                  textStyle: GoogleFonts.inriaSans(
+                    fontSize:  MediaQuery.of(context).size.width > 350 ? 20 : 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  elevation: 5,
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                },
+                child: Text(
+                  "Log out",
+                  textAlign: TextAlign.left,
+                )
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
