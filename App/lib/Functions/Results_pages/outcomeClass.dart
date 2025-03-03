78fac854e065623c9ac964f956d7f0f4590e8c33
PulinduYK <w2052925@westminster.ac.uk>
2025-03-03 21:00:49 +0530

    Add snake Names to output method

--- App/lib/Functions/Results_pages/outcomeClass.dart
+++ App/lib/Functions/Results_pages/outcomeClass.dart
@@ -1,7 +1,20 @@
 import 'dart:core';
 
 class outcomeClass {
-  List<String> snakes = ["Banded Krait"];
+  List<String> snakes = [
+    "Banded Krait",
+    "Common Krait",
+    "Common Sand Boa",
+    "Green Pit Viper",
+    "Green Vine Snake",
+    "Hump Nosed Viper",
+    "Indian Cobra",
+    "Indian Rock Python",
+    "Rat Snake",
+    "Russell's Viper",
+    "Saw Scaled Viper",
+    "Sri Lanka Cat Snake",
+  ];
   List<String> spiders = [];
   List<String> insects = [];
 
