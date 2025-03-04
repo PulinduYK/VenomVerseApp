d7601c12a34ac3fa0789cb8f72c9255d3f96a141
Prabhashan19 <160271718+Prabhashan19@users.noreply.github.com>
2025-03-05 00:15:15 +0530

    Revert "Update gallery permissions"
    This reverts commit 2df0c797a61263e9494d4d1717adf9a5ad540d3b.

--- App/lib/Functions/scan/gallery.dart
+++ App/lib/Functions/scan/gallery.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:photo_manager/photo_manager.dart';
-import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:photo_manager/photo_manager.dart';
+
+import '../Results_pages/result_screen.dart';
 
 class UploadImagesPage extends StatefulWidget {
   @override
@@ -11,8 +14,7 @@ class UploadImagesPage extends StatefulWidget {
 }
 
 class _UploadImagesPageState extends State<UploadImagesPage> {
-  List<AssetEntity> _galleryImages =
-  []; // This is Where the received images of gallery is saved
+  List<AssetEntity> _galleryImages = [];
   File? _selectedImage;
   bool _isUploading = false;
 
@@ -22,28 +24,21 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
     _loadGalleryImages();
   }
 
-  Future<void> _checkAndRequestPermissions() async {
-    final PermissionState permission =
-    await PhotoManager.requestPermissionExtend();
-
-    if (permission.isAuth) {
-      _loadGalleryImages();
-    } else {
-      debugPrint("Permission denied. Please allow access to media.");
-      PhotoManager.openSetting();
-    }
-  }
-
   Future<void> _loadGalleryImages() async {
-    final albums = await PhotoManager.getAssetPathList(type: RequestType.image);
-    if (albums.isNotEmpty) {
-      _galleryImages = await albums.first.getAssetListPaged(page: 0, size: 50);
-      setState(() {});
+    if ((await PhotoManager.requestPermissionExtend()).isAuth) {
+      final albums =
+          await PhotoManager.getAssetPathList(type: RequestType.image);
+      if (albums.isNotEmpty) {
+        _galleryImages =
+            await albums.first.getAssetListPaged(page: 0, size: 50);
+        setState(() {});
+      }
+    } else {
+      debugPrint("Permission Denied");
     }
   }
 
   Future<void> _selectImage(AssetEntity asset) async {
-    // Selecting an image from the gallery and storing it as a image
     final file = await asset.file;
     if (file != null) setState(() => _selectedImage = file);
   }
@@ -56,7 +51,7 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
     var uri = Uri.parse('http://142.93.212.199/predict');
     var request = http.MultipartRequest('POST', uri);
     request.files
-        .add(await http.MultipartFile.fromPath('image', _selectedImage!.path));
+        .add(await http.MultipartFile.fromPath('file', _selectedImage!.path));
 
     try {
       var response = await request.send();
@@ -76,8 +71,16 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
         // Example of using the stored variable
         print('Uploaded Image Data: $uploadedImageData');
 
-        setState(() => _selectedImage =
-        null); // Clear the selection after uploading it to the server
+        setState(() => _selectedImage = null); // Clear selection after upload
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResultScreen(
+              uploadedImageData: uploadedImageData,
+            ),
+          ),
+        );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Failed to upload image.')),
@@ -136,30 +139,30 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
                         final file = snapshot.data;
                         final isSelected = file?.path == _selectedImage?.path;
                         return snapshot.connectionState ==
-                            ConnectionState.done &&
-                            file != null
+                                    ConnectionState.done &&
+                                file != null
                             ? Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.file(file,
-                                  width: double.infinity,
-                                  height: double.infinity,
-                                  fit: BoxFit.cover),
-                            ),
-                            if (isSelected)
-                              const Positioned(
-                                top: 5,
-                                right: 5,
-                                child: CircleAvatar(
-                                  radius: 14,
-                                  backgroundColor: Colors.green,
-                                  child: Icon(Icons.check,
-                                      color: Colors.white),
-                                ),
-                              ),
-                          ],
-                        )
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image.file(file,
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        fit: BoxFit.cover),
+                                  ),
+                                  if (isSelected)
+                                    const Positioned(
+                                      top: 5,
+                                      right: 5,
+                                      child: CircleAvatar(
+                                        radius: 14,
+                                        backgroundColor: Colors.green,
+                                        child: Icon(Icons.check,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                ],
+                              )
                             : _placeholderImage();
                       },
                     ),
@@ -182,48 +185,48 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
   }
 
   Widget _placeholderImage() => Container(
-    decoration: BoxDecoration(
-      color: Colors.grey[300],
-      borderRadius: BorderRadius.circular(12),
-    ),
-  );
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(12),
+        ),
+      );
 
   Widget _uploadButton() => Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-    child: ElevatedButton(
-      onPressed:
-      _selectedImage != null && !_isUploading ? _uploadImage : null,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24.0)),
-        disabledBackgroundColor: Colors.grey.shade300,
-        foregroundColor:
-        _selectedImage != null ? Colors.white : Colors.grey.shade600,
-      ),
-      child: Ink(
-        decoration: BoxDecoration(
-          gradient: _selectedImage != null
-              ? const LinearGradient(
-            colors: [Colors.blueAccent, Colors.purpleAccent],
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-          )
-              : null,
-          borderRadius: BorderRadius.circular(24.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: ElevatedButton(
+          onPressed:
+              _selectedImage != null && !_isUploading ? _uploadImage : null,
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0)),
+            disabledBackgroundColor: Colors.grey.shade300,
+            foregroundColor:
+                _selectedImage != null ? Colors.white : Colors.grey.shade600,
+          ),
+          child: Ink(
+            decoration: BoxDecoration(
+              gradient: _selectedImage != null
+                  ? const LinearGradient(
+                      colors: [Colors.blueAccent, Colors.purpleAccent],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    )
+                  : null,
+              borderRadius: BorderRadius.circular(24.0),
+            ),
+            child: Container(
+              alignment: Alignment.center,
+              height: 50,
+              child: _isUploading
+                  ? const CircularProgressIndicator(color: Colors.white)
+                  : Text('Upload',
+                      style: GoogleFonts.roboto(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
+            ),
+          ),
         ),
-        child: Container(
-          alignment: Alignment.center,
-          height: 50,
-          child: _isUploading
-              ? const CircularProgressIndicator(color: Colors.white)
-              : Text('Upload',
-              style: GoogleFonts.roboto(
-                  fontSize: 16, fontWeight: FontWeight.bold)),
-        ),
-      ),
-    ),
-  );
+      );
 }
 
 void main() => runApp(
