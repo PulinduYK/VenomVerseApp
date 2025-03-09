bfe20d78e88eb7833546621500d7dfb6f15337ca
Thevindu Guruge <tnmdc2003@gmail.com>
2025-03-09 22:08:38 +0530

    Update help page

--- App/lib/Profile/pages/help_page.dart
+++ App/lib/Profile/pages/help_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:venomverse/Profile/widgets/profile_page_template.dart';
 
+
 // HelpPage displays the help details.
 class HelpPage extends StatelessWidget {
   const HelpPage({super.key});
@@ -8,55 +9,55 @@ class HelpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ProfilePageTemplate(
-        title: "Security and Help",
-        contentHeightFactor: 0.85,
-          child: Column(
-            children:[
-              SizedBox(height: 50),
-              CustomButton(
-                text: "Emergency Contact",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "FAQs",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "Customer support",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "Password Management",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "2-Factor Authentication",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "Device Management",
-                onPressed: (){
-                },
-              ),
-              SizedBox(height: 20),
-              CustomButton(
-                text: "Privacy Settings",
-                onPressed: (){
-                },
-              ),
-
-            ],
+      title: "Security and Help",
+      contentHeightFactor: 0.85,
+      child: Column(
+        children:[
+          SizedBox(height: 50),
+          CustomButton(
+            text: "Emergency Contact",
+            onPressed: (){
+            },
           ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "FAQs",
+            onPressed: (){
+            },
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "Customer support",
+            onPressed: (){
+            },
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "Password Management",
+            onPressed: (){
+            },
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "2-Factor Auth",
+            onPressed: (){
+            },
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "Device Manager",
+            onPressed: (){
+            },
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            text: "Privacy Settings",
+            onPressed: (){
+            },
+          ),
+
+        ],
+      ),
     );
   }
 }
