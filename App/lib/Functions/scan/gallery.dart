de7b76b62ee03dd57eae8381bce7dc3284e4f653
Prabhashan19 <160271718+Prabhashan19@users.noreply.github.com>
2025-03-20 23:54:35 +0530

    Changes to the UI
    Changed some UI components in gallery.dart.

--- App/lib/Functions/scan/gallery.dart
+++ App/lib/Functions/scan/gallery.dart
@@ -227,7 +227,8 @@ class _UploadImagesPageState extends State<UploadImagesPage> {
                   ? const LinearGradient(
                       colors: [
                         Color(0xFF1C16B9),
-                        Color(0xFFDC9FDA),
+                        Color(0xff6D5FD5),
+                        Color(0xff8A7FD6),
                       ],
                       begin: Alignment.centerLeft,
                       end: Alignment.centerRight,
