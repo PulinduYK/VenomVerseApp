eb08774185c20e76369c15eb5baf45027a3af976
PulinduYK <w2052925@westminster.ac.uk>
2025-03-23 03:29:00 +0530

    bug fix

--- App/test/widget_test.dart
+++ App/test/widget_test.dart
@@ -14,7 +14,7 @@ void main() {
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Allow the app to initialize
     await tester.pumpAndSettle();
